04fd9e0a2873eaedf4786ecc9071e4c56c9d3e9c
Bram Gotink <bram@gotink.me>
2016-01-05 23:05:56 +0100

    improve readability of src/output

--- src/output/html.js
+++ src/output/html.js
@@ -3,24 +3,31 @@
 const jade = require('jade');
 const fs = require('../utils/fs');
 const path = require('path');
+const _ = require('lodash');
 
-const JADE_DIRECTORY = path.resolve(__dirname, '../../lib/html');
+const JADE_MAIN_FILE = path.resolve(__dirname, '../../lib/html/main.jade');
+
+const getRenderFunction = _.once(function createRenderFunction() {
+  return new Promise(function(resolve, reject) {
+    const jadeRender = jade.compileFile(JADE_MAIN_FILE, {
+      pretty: true
+    });
+
+    resolve(function render(character) {
+      return jadeRender({ character });
+    });
+  });
+});
 
 module.exports = function formatHTML(filename, character) {
-  return new Promise(function(resolve, reject) {
-    resolve(jade.compileFile(
-      path.join(JADE_DIRECTORY, 'main.jade'), {
-        pretty: true,
-      }
-    )({
-      character
-    }));
+  return getRenderFunction()
+  .then(function (render) {
+    return render(character);
   })
   .then(function (html) {
-    const outputFilename = filename.replace(/(\.character)?$/, '.html');
+    const outputFilename = filename.replace(/(\.character)$/, '.html');
+
     return fs.writeFile(outputFilename, html)
-    .then(function () {
-      return outputFilename;
-    });
+    .then(_ => outputFilename);
   });
 };
--- src/output/pdf.js
+++ src/output/pdf.js
@@ -7,31 +7,41 @@ const _ = require('lodash');
 
 const TEX_DIRECTORY = path.resolve(__dirname, '../../lib/tex');
 
-module.exports = function formatPDF(filename, character) {
-  return fs.readFile(path.join(TEX_DIRECTORY, 'template.tex-template'))
+const TEX_MAIN_FILE = path.join(TEX_DIRECTORY, 'template.tex-template');
+const TEX_INCLUDE_DIRECTORY = path.join(TEX_DIRECTORY, 'includes');
+
+const TEMPLATE_OPTIONS = Object.freeze({
+  escape: /<\{-([\s\S]+?)\}>/g,
+  evaluate: /<\{([\s\S]+?)\}>/g,
+  interpolate: /<\{=([\s\S]+?)\}>/g,
+});
+
+const TEX_OPTIONS = Object.freeze({
+  format: 'pdf',
+});
+
+const readTemplate = _.once(function readTemplate() {
+  return fs.readFile(TEX_MAIN_FILE)
   .then(function (template) {
-    return _.template(template, {
-      escape: /<\{-([\s\S]+?)\}>/g,
-      evaluate: /<\{([\s\S]+?)\}>/g,
-      interpolate: /<\{=([\s\S]+?)\}>/g,
-    })({
+    return _.template(template, TEMPLATE_OPTIONS);
+  });
+});
+
+module.exports = function formatPDF(filename, character) {
+  return readTemplate()
+  .then(function (template) {
+    return template({
       character,
-      includeDirectory: path.join(TEX_DIRECTORY, 'includes'),
+      includeDirectory: TEX_INCLUDE_DIRECTORY,
     });
   })
   .then(function (latexSource) {
-    return latex(latexSource, {
-      format: 'pdf',
-    });
-  })
-  .then(function (pdfStream) {
-    const outputFilename = filename.replace(/(\.character)?$/, '.pdf');
+    const outputFilename = filename.replace(/(\.character)$/, '.pdf');
     console.log('Typesetting file %s', outputFilename);
+
     return fs.writeStreamToFile(
       outputFilename,
-      pdfStream
-    ).then(function () {
-      return outputFilename;
-    });
+      latex(latexSource, TEX_OPTIONS)
+    ).then(_ => outputFilename);
   });
 };
